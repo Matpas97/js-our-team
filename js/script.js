4da81f693ca1991f2e2ca0d827d448fa7dd0f6b4
Matpas97 <passarellim22@gmail.com>
2022-09-06 16:29:37 +0200

    Fine Esercizio

--- js/script.js
+++ js/script.js
@@ -1,76 +1,97 @@
-
-// Viene fornito un layout di base in cui è presente una card di esempio inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e dovrà quindi essere rimossa dall’html.
-// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
-// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html
-
-// Aggiungo un Array di oggetti che rapprresentano i membri del team 
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
 
 let objectArray =[
 
-    Mebro1 = {
+     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         image: 'wayne-barnett-founder-ceo',
+        
     },
 
-    Membro2 = {
+    {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         image: 'angela-caroll-chief-editor',
 
     },
 
-    Membro3 = {
+    {
         name: 'Walter Gordon',
         role: 'Office Manage',
         image: 'walter-gordon-office-manager',
     },
 
-    Membro4 = {
+     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         image: 'angela-lopez-social-media-manager',
     },
 
-    Membro5 = {
+     {
         name: 'Scott Estrada',
         role: 'Developer',
         image: 'scott-estrada-developer',
     },
 
-    Membro6 = {
+    {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
         image: 'barbara-ramos-graphic-designer',
     }
    
-
 ];
-
-// Seleziono la classe del dom da popolare successivamente
 let teamContainer = document.querySelector('.team-container');
 
-// Creo un ciclo for per ogni card dell'Array
-for(let i = 0; i < objectArray; i++) {
-     const thiscard = objectArray[i];
+// Creo un ciclo for dove stampo in console ruolo nome e img e li stampo anche nel dom
+
+for (let index = 0; index < objectArray.length; index++) {
+    const element = objectArray[index];
+    console.log(element);
+
+    // Creo gli elementi del Dom 
+    
+    let cardAdd = `
+    <div class= "team-card">
+     <div class= "card-image">
+       <img 
+        src="img/${element.image}.jpg"
+        alt= "${element.name}"/>
+        </div>
+         <div class="card-text">
+         <h3>${element.name}</h3>
+         <p>${element.role}</p>
+         </div>
+         </div>`;
+
+   teamContainer.innerHTML +=  cardAdd;     
+
 }
 
-// Creo un nuovo template che riempio con i dati corretti 
- 
-let cardAdd = `
-<div class= "team-card">
- <div class= "card-image">
-   <img 
-    src="img/${thiscard.image}.jpg
-    alt= "${thiscard.name}"/>
-    </div>
-     <div class="card-text">
-     <h3>${thiscard.name}</h3>
-     <p>${thiscard.role}</p>
-     </div>
-     </div>`;
 
-// Concateno alla classe selezionata
-teamContainer.innerHTML += objectArray;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
